7ee09fd310c9fdb025859374250a871a7999e95f
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-29 12:51:20 -0500

    Basic taskbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import type { Component } from 'solid-js';
 import Window from './Window';
+import TaskBar from './TaskBar';
 
 const App: Component = () => {
   return (
     <>
       <h1>Hello world!!!!</h1>
       <Window />
+      <TaskBar />
     </>
   );
 };
--- src/Window.tsx
+++ src/Window.tsx
@@ -3,7 +3,7 @@ import { createDraggable } from "@neodrag/solid";
 export default () => {
     const {draggable} = createDraggable();
 
-  return <div use:draggable style="border: 2px solid black; width: 500px ; height: 500px">
+  return <div use:draggable style="border: 2px solid black; width: 500px ; height: 500px ; background: white">
     Window
   </div>;
 };
