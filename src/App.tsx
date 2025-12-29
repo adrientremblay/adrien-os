2484850f3de7f70765e64b6cca44ec47a68daed7
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-29 14:29:36 -0500

    Tweaked wallpaper color

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ const App: Component = () => {
       <svg height="100%" width="100%" style="position:fixed; left: 0 ; top: 0 ; z-index: -100">
         <defs>
           <linearGradient id="gr1" x1="0%" y1="60%" x2="100%" y2="0%">
-            <stop offset="5%" style="stop-color:rgb(255,255,3);stop-opacity:1" />
-            <stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+            <stop offset="5%" style="stop-color:rgba(159, 152, 255, 1);stop-opacity:1" />
+            <stop offset="100%" style="stop-color:rgba(52, 49, 54, 1);stop-opacity:1" />
           </linearGradient>
         </defs>
         <rect width="100%" height="100%" x="0" y="0" fill="url(#gr1)" />
