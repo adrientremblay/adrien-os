de314f239c3624e78e7933e611bb30a24fe0600a
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-29 13:21:07 -0500

    Basic dragging using interact.js

--- src/Window.tsx
+++ src/Window.tsx
@@ -1,9 +1,24 @@
-import { createDraggable } from "@neodrag/solid";
+import interact from 'interactjs'
 
 export default () => {
-    const {draggable} = createDraggable();
 
-  return <div use:draggable style="border: 2px solid black; width: 500px ; height: 500px ; background: white">
+    const position = {x: 0, y: 0};
+
+    interact('.draggable').draggable({
+        listeners: {
+            start: (e) => {
+                console.log(e.type, e.target);
+            },
+            move: (e) => {
+                position.x += e.dx;
+                position.y += e.dy;
+                e.target.style.transform =
+                    `translate(${position.x}px, ${position.y}px)`
+            }
+        }
+    })
+
+  return <div class="draggable" style="border: 2px solid black; width: 500px ; height: 500px ; background: white">
     Window
   </div>;
 };
