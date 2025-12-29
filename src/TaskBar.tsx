528f4a362a40cf7e955f57412bd8029c1baf5038
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-29 14:20:38 -0500

    added a bunch more icons

--- src/TaskBar.tsx
+++ src/TaskBar.tsx
@@ -2,6 +2,11 @@ export default () => {
     return <div class='taskbar'>
         <div style="margin-left: auto ; margin-right: 20px; float: right">
             <img src="/images/wifi_icon.png" width="32px" height="32px"></img>
+            <img src="/images/bluetooth.png" width="32px" height="32px"></img>
+            <img src="/images/information.png" width="32px" height="32px"></img>
+            <img src="/images/update.png" width="32px" height="32px"></img>
+            <img src="/images/volume.png" width="32px" height="32px"></img>
+            <img src="/images/weather.png" width="32px" height="32px"></img>
         </div>
     </div>;
 }
