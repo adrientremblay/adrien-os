eb72f6cf74cdac245719ce2284a41b4fc697619e
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-29 14:26:05 -0500

    added shutdown icon to taskbar

--- src/TaskBar.tsx
+++ src/TaskBar.tsx
@@ -1,6 +1,9 @@
 export default () => {
     return <div class='taskbar'>
-        <div style="margin-left: auto ; margin-right: 20px; float: right">
+        <div style="margin-left: auto ; margin-left: 10px; float: left">
+            <img src="/images/shutdown.png" width="32px" height="32px"></img>
+        </div>
+        <div style="margin-left: auto ; margin-right: 10px; float: right">
             <img src="/images/wifi_icon.png" width="32px" height="32px"></img>
             <img src="/images/bluetooth.png" width="32px" height="32px"></img>
             <img src="/images/information.png" width="32px" height="32px"></img>
